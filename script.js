8ac611501f4a220d053b7d3d1ab448278e4b5d19
MaxraySavage <MaxraySavage@users.noreply.github.com>
2021-08-22 21:46:37 -0400

    completes color mode choice

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const gridContainer = document.getElementById("grid-container");
 const leftDial = document.getElementById('dial-left');
 const rightDial = document.getElementById('dial-right');
 let totalDivs;
+let randomColors = false;
+
+// Input an string that holds the color values for an html element
+// Return the three values as numbers in an array 
+function toRGBArray (rgbStyleString) {
+    return rgbStyleString.split('(')[1].slice(0,-1).split(',').map((a) => Number(a));
+}
+
+// Take an array with three elements and sets each element to the
+// Weighted average between those elements and corresponding ones in a target array
+// With the target arrays values weighted by targetWeight
+// This lets us move color values towards background color
+// Used for etchasketch shake color changes
+function moveTowardsTargetColor(rbgArray, rgbTarget=[190,190,190], targetWeight = 1) {
+    for(let i = 0; i < 3; i++) {
+        rbgArray[i] = Math.round((rbgArray[i] + rgbTarget[i] * targetWeight) / (targetWeight + 1));
+    }
+}
 
 function setGridDimensions(numRows, numCols) {    
     while (gridContainer.firstChild) {
@@ -15,7 +33,20 @@ function setGridDimensions(numRows, numCols) {
         const div = document.createElement('div');
         div.className = "grid-square";
         div.addEventListener("mouseenter", function changeColor(e) {
-            e.target.style.backgroundColor = 'rgb(80,80,80)'
+            if(randomColors) {
+                let r = Math.floor(Math.random() * 256);
+                let g = Math.floor(Math.random() * 256)
+                let b = Math.floor(Math.random() * 256)
+                e.target.style.backgroundColor = `rgb(${r},${g},${b})`
+            } else {
+                let rgbString = e.target.style.backgroundColor;
+                if (rgbString === '') {
+                    rgbString= 'rgb(190,190,190)';
+                }
+                rgbVals = toRGBArray(rgbString);
+                moveTowardsTargetColor(rgbVals, [30,30,30], 1);
+                e.target.style.backgroundColor = `rgb(${rgbVals[0]},${rgbVals[1]},${rgbVals[2]})`
+            }
         });
         docFrag.appendChild(div);
     }
@@ -27,9 +58,9 @@ function shakeEtchASketch() {
     gridContainer.childNodes.forEach((child) => {
         let currentBackground = child.style.backgroundColor;
         if(currentBackground.includes('rgb')) {
-            let currentRGB = Number(currentBackground.split('(')[1].split(',')[0]);
-            let nextRGB = Math.ceil(.5 * Math.abs(destinationRGB - currentRGB) + currentRGB);
-            child.style.backgroundColor = `rgb(${nextRGB},${nextRGB},${nextRGB})`;
+            currentRGB = toRGBArray(currentBackground);
+            moveTowardsTargetColor(currentRGB);
+            child.style.backgroundColor = `rgb(${currentRGB[0]},${currentRGB[1]},${currentRGB[2]})`;
         }
     });
 }
@@ -54,13 +85,22 @@ window.addEventListener('load', () => {
             etchASketch.style.transform = 'rotate(0deg)'
             etchASketch.style.transform = 'transform(0px)'
         }, 500)
-    })
-});
-
-const slider = document.getElementById('slider');
-slider.addEventListener('input', (e) => {
+    });
+    document.getElementById('random').addEventListener('input',()=>{
+        randomColors = true;
+    });
+    document.getElementById('black').addEventListener('input',()=>{
+        randomColors = false;
+    });
+    document.getElementById('black').click();
+    const slider = document.getElementById('slider');
+    slider.addEventListener('input', (e) => {
     setGridDimensions(e.target.value * 3, e.target.value * 5);
 });
+
+});
+
+
 
 
 
